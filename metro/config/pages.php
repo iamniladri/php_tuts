0bd2079b5951dbd2e928d665af20cd96d8013735
iamniladri <niladri.professional@yahoo.in>
2013-11-15 23:49:11 +0530

    Initial commit

---
+++ metro/config/pages.php
@@ -0,0 +1,21 @@
+<?php
+/*
+All pages you have in the /pages/ folder must be defined here, it's their own title (shown on top and in the url)
+
+ex: $pageTitles['file.php'] = "This is the title";
+
+Just copy paste this and fill in :
+
+$pageTitles[''] = "";
+
+/* PAGE OPTIONS */
+
+$pageTitles['welcome.php'] = "Welcome";
+$pageTitles['typography.php'] = "Typography";
+$pageTitles['accordions.php'] = "Accordions";
+$pageTitles['sidebars.php'] = "Sidebars";
+
+$pageDescriptions['welcome.php'] = "This is the welcome page. Welcome.";
+
+$pageKeywords['welcome.php'] = "welcome, page, test";
+?>
